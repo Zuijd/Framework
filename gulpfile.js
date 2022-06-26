1a9c747e274fcfecd246f706e46af32058d42e7b
Zuijd <dca.vanzuijdam@student.avans.nl>
2022-06-26 15:50:52 +0200

    added and changed button classes

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('pocahontas/scss/**/*.scss')
-    .pipe(sass())/* 
-    .pipe(purgecss({ content: ['*.html'] })) */
-    .pipe(dest('pocahontas/dist/css'))
+  return src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('dist/css'))
 }
 
 function watchTask() {
-  watch(['pocahontas/scss/**/*.scss', '*.html'], buildStyles)
+  watch(['scss/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
